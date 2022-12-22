f915dec7fe7cf7daec78cc610127c436dbe920ae
gayatriCodes23 <121214507+gayatriCodes23@users.noreply.github.com>
2022-12-22 11:46:53 +0530

    Update main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,7 +2,7 @@ package application;
 
 public class Main {
 	public static void main(String[] args) {
-		Account abhaysAccount= new Account("545545", 0.0, "mohan", "gayatri@gmail.com", "9600096000");
+		Account abhaysAccount= new Account("545545", 0.0, "abhay", "abhay @gmail.com", "9600096000");
 		System.out.println("Welcome " + abhaysAccount.getName());
 		abhaysAccount.depositeMoney(500);
 		abhaysAccount.withdrawMoney(200);
